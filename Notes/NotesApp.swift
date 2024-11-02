3b668841aa90d36c9b97ed6e5cc14c4866d0e67c
cyril-fl <cyril-f@hotmail.com>
2024-11-02 02:02:30 +0100

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Cyril Flambard on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,16 @@
+//
+//  NotesTests.swift
+//  NotesTests
+//
+//  Created by Cyril Flambard on 02/11/2024.
+//
+
+import Testing
+
+struct NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
